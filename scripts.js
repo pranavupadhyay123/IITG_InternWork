ec8fdd452f4cb94ea179cdabb9dead4c24a1703e
Kavita Dhar <dharkavita123@gmail.com>
2024-07-19 19:41:30 +0530

    projects

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//project
+// Dark & Light toggle
+
+document.querySelector(".day-night input").addEventListener("change", () => {
+    document.querySelector("body").classList.add("toggle");
+    setTimeout(() => {
+      document.querySelector("body").classList.toggle("light");
+  
+      setTimeout(
+        () => document.querySelector("body").classList.remove("toggle"),
+        10
+      );
+    }, 5);
+  });
+  
